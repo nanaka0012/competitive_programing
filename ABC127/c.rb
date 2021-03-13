73b04f5c05014bff43f5ba8259d045d973754d2f
nanaka0012 <nanaka0012@yahoo.co.jp>
2021-03-14 01:05:16 +0900

    update: ABC127 c

--- ABC127/c.rb
+++ ABC127/c.rb
@@ -1,7 +1,38 @@
+# n, m = gets.chomp.split.map(&:to_i)
+# card = [*(1..n)]
+# ans = 0
+# m.times do
+#   l, r = gets.chomp.split.map(&:to_i)
+#   pass = [*(l..r)]
+#   card.each do |n|
+#     if !pass.include?(n)
+#       card.delete(n)
+#     end
+#   end
+# end
+
+# puts card.size
+
+# *********
 n, m = gets.chomp.split.map(&:to_i)
-card = [*(1..n)]
-gate = []
+
+min = 1
+max = n
+
 m.times do
   l, r = gets.chomp.split.map(&:to_i)
-  gate << []
+  if min < l
+    min = l
+  end
+
+  if max > r
+    max = r
+  end
+
+  if min > max
+    puts 0
+    return
+  end
 end
+
+puts max - min + 1
